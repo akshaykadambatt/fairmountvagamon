0d17bf8ae5a3775086a07d1af31ed8fb1bf85a44
Akshay <akshay@spawoz.com>
2022-12-16 00:45:06 +0530

    Random commit

--- components/data/firebaseConfig.ts
+++ components/data/firebaseConfig.ts
@@ -5,16 +5,6 @@ import 'firebase/compat/analytics';
 import { getStorage } from 'firebase/storage';
 // import { getFunctions } from 'firebase/functions';
 
-const config = {
-    apiKey: "AIzaSyA9ojLeMwtCxn27_MOWCEvG8HZHe_04YmY",
-  authDomain: "fairmountvagamonweb.firebaseapp.com",
-  projectId: "fairmountvagamonweb",
-  storageBucket: "fairmountvagamonweb.appspot.com",
-  messagingSenderId: "719645493551",
-  appId: "1:719645493551:web:3163418be0ccb7b88c39da",
-  measurementId: "G-9C3J4CY5NH"
-};
-
 const aapp = firebase.initializeApp(config)
 // window.self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 export const auth = aapp.auth()
