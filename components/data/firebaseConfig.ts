1dcdb8532626561e8ded00458e14a2b8bb34a9b4
Akshay <akshay@spawoz.com>
2022-12-16 00:49:03 +0530

    ENV set

--- components/data/firebaseConfig.ts
+++ components/data/firebaseConfig.ts
@@ -5,6 +5,16 @@ import 'firebase/compat/analytics';
 import { getStorage } from 'firebase/storage';
 // import { getFunctions } from 'firebase/functions';
 
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+};
+
 const aapp = firebase.initializeApp(config)
 // window.self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 export const auth = aapp.auth()
