c766d0708cc525f1a5725f71f6a0854aa9c4a11f
Akshay <akshay@spawoz.com>
2022-12-22 11:01:11 +0530

    Manage products

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -12,6 +12,7 @@ import {
   TextInput,
   Menu,
   Avatar,
+  Modal,
 } from "@mantine/core";
 import {
   IconNotes,
@@ -33,50 +34,17 @@ import {
   IconCalendarPlus,
   IconUserX,
   IconUsers,
+  IconGif,
+  IconGitBranch,
 } from "@tabler/icons";
 import { UserButton } from "./UserButton";
 import { LinksGroup } from "./NavbarLinksGroup";
-import { ReactChildren, useEffect, useContext } from "react";
-import router from "next/router";
+import { ReactChildren, useEffect, useContext, useState } from "react";
+import router, { useRouter } from "next/router";
 import { auth } from "../components/data/firebaseConfig";
 import { AuthContext } from "./data/AuthContext";
 import Link from "next/link";
-
-const mockdata = [
-  { label: "Dashboard", icon: IconGauge, link: "dashboard" },
-  {
-    label: "Pages",
-    icon: IconNotes,
-    initiallyOpened: true,
-    links: [
-      { label: "Pages", link: "pages" },
-      { label: "Add New Page", link: "/" },
-      { label: "Gallery", link: "/" },
-      { label: "Contacts", link: "/" },
-    ],
-  },
-  {
-    label: "Products",
-    icon: IconBuildingStore,
-    initiallyOpened: false,
-    links: [
-      { label: "Products", link: "products" },
-      { label: "Add New Product", link: "/" },
-    ],
-  },
-  {
-    label: "Bookings",
-    icon: IconCalendarPlus,
-    initiallyOpened: false,
-    links: [
-      { label: "Bookings", link: "/" },
-      { label: "Modify availability", link: "/" },
-      { label: "Users", link: "/" },
-    ],
-  },
-  { label: "Testimonials", icon: IconUsers, link: "testimonials" },
-  { label: "Go to website", icon: IconPresentationAnalytics, link: "/" },
-];
+import Head from "next/head";
 
 const useStyles = createStyles((theme) => ({
   navbar: {
@@ -114,8 +82,48 @@ const useStyles = createStyles((theme) => ({
 }));
 type Props = { children: React.ReactNode };
 const Navigation: React.FC<Props> = ({ children }) => {
+  const { asPath, pathname } = useRouter();
+  const [openDevModal, setOpenDevModal] = useState(false);
+  const [devModalData, setDevModalData] = useState("");
+
   const { classes } = useStyles();
   const user = useContext(AuthContext);
+
+  const mockdata = [
+    { label: "Dashboard", icon: IconGauge, link: "dashboard" },
+    {
+      label: "Pages",
+      icon: IconNotes,
+      initiallyOpened: pathname?.match("admin/pages")?.length || 0 > 0 ? true : false,
+      links: [
+        { label: "Pages", link: "pages" },
+        { label: "Add New Page", link: "/" },
+        { label: "Gallery", link: "/" },
+        { label: "Contacts", link: "/" },
+      ],
+    },
+    {
+      label: "Products",
+      icon: IconBuildingStore,
+      initiallyOpened: pathname?.match("admin/products")?.length || 0 > 0 ? true : false,
+      links: [
+        { label: "Products", link: "/admin/products" },
+        { label: "Add New Product", link: "/admin/products/manage-product" },
+      ],
+    },
+    {
+      label: "Bookings",
+      icon: IconCalendarPlus,
+      initiallyOpened: pathname?.match("admin/bookings")?.length || 0 > 0 ? true : false,
+      links: [
+        { label: "Bookings", link: "/" },
+        { label: "Modify availability", link: "/" },
+        { label: "Users", link: "/" },
+      ],
+    },
+    { label: "Testimonials", icon: IconUsers, link: "testimonials" },
+    { label: "Go to website", icon: IconPresentationAnalytics, link: "/" },
+  ];
   const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
   const signOut = async () => {
     await auth.signOut();
@@ -127,46 +135,74 @@ const Navigation: React.FC<Props> = ({ children }) => {
         router.push("/admin");
       }
     });
+    setDevModalData("");
+    //https://api.github.com/repos/akshayknz/fairmountvagamon/commits?sha=main&per_page=1&page=1
+    fetch("https://api.github.com/repos/akshayknz/fairmountvagamon/commits")
+      .then((r) => r.json())
+      .then((r) => setDevModalData(r));
   }, []);
   return (
-    <Grid gutter={0}>
-      <Grid.Col span="content">
-        <Navbar height={"100vh"} width={{ sm: "100%" }} p="md" pb={0} className={classes.navbar}>
-          <Navbar.Section className={classes.header}>
-            <Group position="apart">
-              <Text>Fairmount Resorts</Text>
-              <Code sx={{ fontWeight: 700 }}>v1.0</Code>
-            </Group>
-          </Navbar.Section>
-          <Navbar.Section grow className={classes.links} component={ScrollArea}>
-            <div className={classes.linksInner}>{links}</div>
-          </Navbar.Section>
-          <Navbar.Section className={classes.footer}>
-            <Menu shadow="md" width={200} position="right-end" transition={"pop-top-left"}>
-              <Menu.Target>
-                <Box>
-                  <UserButton
-                    image={user?.photoURL ? user.photoURL : ""}
-                    name={user?.displayName ? user.displayName : ""}
-                    email={user?.email ? user.email : ""}
-                  />
-                </Box>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Link href="/" target={"_blank"}>
-                  <Menu.Item icon={<IconArrowUpRight size={14} />}>Visit website</Menu.Item>
-                </Link>
-                <Menu.Label>Account Settings</Menu.Label>
-                <Menu.Item color="red" icon={<IconLogout size={14} />} onClick={signOut}>
-                  Logout
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          </Navbar.Section>
-        </Navbar>
-      </Grid.Col>
-      <Grid.Col span={9}>{children}</Grid.Col>
-    </Grid>
+    <>
+      <Head>
+        <title>Fairmount Restorts: Admin</title>
+        <meta name="description" content="Fairmount Restorts website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Grid gutter={0}>
+        <Grid.Col span="content">
+          <Navbar height={"100vh"} width={{ sm: "100%" }} p="md" pb={0} className={classes.navbar}>
+            <Navbar.Section className={classes.header}>
+              <Group position="apart">
+                <Text>Fairmount Resorts</Text>
+                <Code sx={{ fontWeight: 700 }}>v1.0</Code>
+              </Group>
+            </Navbar.Section>
+            <Navbar.Section grow className={classes.links} component={ScrollArea}>
+              <div className={classes.linksInner}>{links}</div>
+            </Navbar.Section>
+            <Navbar.Section className={classes.footer}>
+              <Menu shadow="md" width={200} position="right-end" transition={"pop-top-left"}>
+                <Menu.Target>
+                  <Box>
+                    <UserButton
+                      image={user?.photoURL ? user.photoURL : ""}
+                      name={user?.displayName ? user.displayName : ""}
+                      email={user?.email ? user.email : ""}
+                    />
+                  </Box>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Link href="/" target={"_blank"}>
+                    <Menu.Item icon={<IconArrowUpRight size={14} />}>Visit website</Menu.Item>
+                  </Link>
+                  <Menu.Item icon={<IconGitBranch size={14} />} onClick={() => setOpenDevModal(true)}>
+                    Developer stats
+                  </Menu.Item>
+                  <Menu.Label>Account Settings</Menu.Label>
+                  <Menu.Item color="red" icon={<IconLogout size={14} />} onClick={signOut}>
+                    Logout
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Navbar.Section>
+          </Navbar>
+        </Grid.Col>
+        <Grid.Col span={9}>{children}</Grid.Col>
+      </Grid>
+      <Modal
+        size={"70vw"}
+        opened={openDevModal}
+        onClose={() => setOpenDevModal(false)}
+        title="Developer data"
+        overflow="inside"
+      >
+          <pre id="json" style={{fontSize:10}}>
+          {JSON.stringify(devModalData, undefined, 2)}
+        </pre>
+        
+      </Modal>
+    </>
   );
 };
 
---
+++ pages/admin/products/manage-product.tsx
@@ -0,0 +1,237 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import {
+  ActionIcon,
+  Badge,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Grid,
+  Group,
+  Modal,
+  NumberInput,
+  Popover,
+  SimpleGrid,
+  Switch,
+  Table,
+  Text,
+  TextInput,
+  Textarea,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import Navigation from "../../../components/Navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "@mantine/form";
+import { doc, collection, setDoc, onSnapshot, query, where, deleteDoc, updateDoc } from "firebase/firestore";
+import { db } from "../../../components/data/firebaseConfig";
+import { CollectionName } from "../../../components/data/constants";
+import {
+  IconChevronDown,
+  IconChevronUp,
+  IconChevronsDown,
+  IconChevronsUp,
+  IconPencil,
+  IconSettings,
+  IconTrash,
+} from "@tabler/icons";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FunctionDeclaration } from "typescript";
+export default function Pages() {
+  const [opened, setOpened] = useState(false);
+  const [testimonials, setTestimonials] = useState<TestimonialProps[]>([]);
+  const [testimonialsEditId, setTestimonialsEditId] = useState("");
+  const theme = useMantineTheme();
+  const form = useForm({
+    initialValues: {
+      name: "",
+      content: "",
+      status: true,
+      order: 0,
+    },
+
+    validate: {
+      name: (value) => (value != "" ? null : "Enter name"),
+      content: (value) => (value != "" ? null : "Enter the body of testimonial"),
+    },
+  });
+  const handleFormSubmit = async (values: TestimonialProps) => {
+    let q;
+    if (testimonialsEditId) {
+      q = doc(db, CollectionName.TESTIMONIALS, testimonialsEditId);
+    } else {
+      q = doc(collection(db, CollectionName.TESTIMONIALS));
+    }
+    await setDoc(q, values);
+    setOpened(false);
+    setTestimonialsEditId("");
+  };
+  const handleEditAction = (data: TestimonialProps) => {
+    console.log(data);
+    form.setFieldValue("name", data.name);
+    form.setFieldValue("content", data.content);
+    form.setFieldValue("status", data.status);
+    form.setFieldValue("id", data.id);
+    setTestimonialsEditId(data.id ? data.id : "");
+    setOpened(true);
+  };
+  
+  const orderShift= async (data: TestimonialProps, type: string) => {
+    let order;
+    switch (type){
+      case "up":
+        order = data.order-1;
+        break;
+      case "down":
+        order = data.order+1;
+        break;
+      default:
+        break;
+    }
+    let q = doc(db, CollectionName.TESTIMONIALS, data.id?data.id:"");
+    await updateDoc(q, {
+      order: order
+    });
+  }
+  useEffect(() => {
+    const unsub1 = onSnapshot(query(collection(db, CollectionName.TESTIMONIALS)), (collectionSnapshot) => {
+      let data: TestimonialProps[] = [];
+      collectionSnapshot.docs.map((doc) => {
+        data.push(Object.assign({ ...doc.data() }, { id: doc.id }) as TestimonialProps);
+      });
+      setTestimonials(data);
+    });
+  }, []);
+  useEffect(() => {
+    if (!opened) form.reset();
+  }, [opened]);
+  const rows = testimonials
+    .sort((a, b) => a.order - b.order)
+    .map((e) => <Row data={e} handleEditAction={handleEditAction} orderShift={orderShift} />);
+  return (
+    <Navigation>
+      <Modal size={"lg"} opened={opened} onClose={() => setOpened(false)} title="Manage Testimonial">
+        
+      </Modal>
+      <Container py={20}>
+        <Box
+          style={{
+            display: "flex",
+            gap: "20px",
+            alignItems: "center",
+          }}
+          pb={20}
+        >
+          <Title order={2} weight={100}>Products</Title>
+          <Button onClick={() => setOpened(true)}>Add Product</Button>
+        </Box>
+        <form
+          onSubmit={form.onSubmit((values) => {
+            handleFormSubmit(values);
+          })}
+        >
+          <TextInput withAsterisk label="Name" placeholder="Enter the name" {...form.getInputProps("name")} />
+          <Textarea
+            placeholder="Testimonail Body"
+            label="Testimonail Body"
+            withAsterisk
+            {...form.getInputProps("content")}
+            autosize
+            minRows={5}
+          />
+          <NumberInput placeholder="Order" label="Order" withAsterisk {...form.getInputProps("order")} />
+          <Switch label="Active" {...form.getInputProps("status", { type: "checkbox" })} />
+          <Group position="right" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+      </Container>
+    </Navigation>
+  );
+}
+interface RowProps {
+  data: TestimonialProps;
+  handleEditAction: (data: TestimonialProps) => void;
+  orderShift: (data: TestimonialProps, type: string) => void;
+}
+const Row = ({ data, handleEditAction, orderShift }: RowProps) => {
+  const theme = useMantineTheme();
+  const [trashPopover, setTrashPopover] = useState(false);
+  const deleteTestimonial = async (id: string) => {
+    console.log(id);
+    deleteDoc(doc(db, CollectionName.TESTIMONIALS, id));
+  };
+  return (
+    <>
+      <tr key={data.id}>
+        <td>
+          {data.name}
+        </td>
+        <td>{data.content}</td>
+        <td>{data.status ? <Badge color="green">Active</Badge> : <Badge color="gray">Inactive</Badge>}</td>
+        <td>
+          <Button.Group>
+            <Button size="xs" px={10} variant="light" onClick={() => orderShift(data, "up")}>
+              <IconChevronUp size={16} />
+            </Button>
+            <Button size="xs" px={10} variant="light" onClick={() => orderShift(data, "down")}>
+              <IconChevronDown size={16} />
+            </Button>
+            <Button size="xs" px={10} variant="light" onClick={() => handleEditAction(data)}>
+              <IconPencil size={16} />
+            </Button>
+            <Popover
+              closeOnClickOutside
+              transition="pop"
+              withArrow
+              withRoles
+              trapFocus
+              position="bottom-end"
+              opened={trashPopover}
+            >
+              <Popover.Target>
+                <Button color="red" size="xs" px={10} variant="light" onClick={() => setTrashPopover((prev) => !prev)}>
+                  <AiOutlineDelete size={16} />
+                </Button>
+              </Popover.Target>
+              <Popover.Dropdown>
+                <Box>
+                  <Text size={"sm"} mb={10}>
+                    Delete this testimonial?
+                  </Text>
+                  <Box>
+                    <Button
+                      size={"sm"}
+                      compact
+                      mr={8}
+                      variant="light"
+                      color="red"
+                      onClick={() => {
+                        deleteTestimonial(data.id ? data.id : "");
+                        setTrashPopover((prev) => !prev);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                    <Button
+                      size={"sm"}
+                      compact
+                      mr={8}
+                      variant="subtle"
+                      onClick={() => setTrashPopover((prev) => !prev)}
+                    >
+                      Cancel
+                    </Button>
+                  </Box>
+                </Box>
+              </Popover.Dropdown>
+            </Popover>
+          </Button.Group>
+        </td>
+      </tr>
+    </>
+  );
+};
