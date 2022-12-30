0ce923939dd804b814f87183077e9e93686c9b19
Jowin John Chemban <29167718+jowinjohnchemban@users.noreply.github.com>
2022-12-30 22:54:46 +0530

    Added SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,7 +143,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Fairmount Resorts</title>
         <link rel="icon" href="/favicon.ico" />
