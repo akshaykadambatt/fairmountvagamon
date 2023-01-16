b03eb7fa4a118eba2476eb738183238092b30522
Akshay <akshay@spawoz.com>
2023-01-15 19:47:19 -0500

    Bugfix faq

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -49,16 +49,14 @@ const useStyles = createStyles((theme) => ({
     borderRadius: theme.radius.md,
     marginBottom: theme.spacing.lg,
 
-    border: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
-      }`,
+    border: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]}`,
   },
 }));
 
 const placeholder =
-  'It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.';
+  "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.";
 
-
-const getChild = (height: number) => <Skeleton height={ height } radius = "md" />;
+const getChild = (height: number) => <Skeleton height={height} radius="md" />;
 const BASE_HEIGHT = 360;
 const getSubHeight = (children: number, spacing: number) =>
   BASE_HEIGHT / children - spacing * ((children - 1) / children);
@@ -78,58 +76,58 @@ export default function Contact() {
 
   return (
     <>
-    <Head>
-    <meta charSet= "utf-8" />
-    <meta name="viewport" content = "width=device-width, initial-scale=1.0" />
-    <title>Welcome to Fairmount - Discover the Idyllic Beauty of Vagamon at Fairmount < /title>
-    < link rel = "icon" type = "image/png" href = "/favicon.ico" />
-    <link rel="apple-touch-icon" sizes = "180x180" href = "/apple-touch-icon.png" />
-    <link rel="icon" type = "image/png" sizes = "32x32" href = "/favicon-32x32.png" />
-    <link rel="icon" type = "image/png" sizes = "16x16" href = "/favicon-16x16.png" />
-    <link rel="manifest" href = "/site.webmanifest" />
-    <meta name="msapplication-TileColor" content = "#FFFFFF" />
-    <meta name="theme-color" content = "#FFFFFF" />
-    <meta
-      name="keywords"
-  content = "resort, Vagamon, Kerala, hill resort, accommodations, tree house, restaurant, room service, spa, outdoor pool, trekking, paragliding, rock climbing, resort at vagamon, fairmount vagamon, fairmount vagamon resorts, resort offers, experience resort, most popular resort, resort experience, resort nature, Vagamon resorts, Kerala hill resorts ,Western Ghats resorts ,Tea plantation resorts ,Nature resorts ,Adventure resorts ,Luxury resorts ,Relaxation resorts ,Spa resorts ,Romantic getaways ,Family vacations ,Hill station vacations ,Hill station getaways ,Western Ghats tourism ,Kerala tourism ,Tea plantation tours ,Nature tours ,Adventure tours ,Luxury travel ,Relaxation vacations ,Spa vacations ,Romantic holidays ,Family holidays ,Honeymoon destinations"
-    />
-    <meta
-      name="description"
-  content = "Escape to the stunning beauty of Vagamon at Fairmount Residency. Our resort offers comfortable accommodations, a range of dining options, and a range of activities and amenities to ensure that our guests have a comfortable and enjoyable stay."
-    />
-    <meta name="author" content = "Fairmount Vagamon" />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Welcome to Fairmount - Discover the Idyllic Beauty of Vagamon at Fairmount </title>
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta
+          name="keywords"
+          content="resort, Vagamon, Kerala, hill resort, accommodations, tree house, restaurant, room service, spa, outdoor pool, trekking, paragliding, rock climbing, resort at vagamon, fairmount vagamon, fairmount vagamon resorts, resort offers, experience resort, most popular resort, resort experience, resort nature, Vagamon resorts, Kerala hill resorts ,Western Ghats resorts ,Tea plantation resorts ,Nature resorts ,Adventure resorts ,Luxury resorts ,Relaxation resorts ,Spa resorts ,Romantic getaways ,Family vacations ,Hill station vacations ,Hill station getaways ,Western Ghats tourism ,Kerala tourism ,Tea plantation tours ,Nature tours ,Adventure tours ,Luxury travel ,Relaxation vacations ,Spa vacations ,Romantic holidays ,Family holidays ,Honeymoon destinations"
+        />
+        <meta
+          name="description"
+          content="Escape to the stunning beauty of Vagamon at Fairmount Residency. Our resort offers comfortable accommodations, a range of dining options, and a range of activities and amenities to ensure that our guests have a comfortable and enjoyable stay."
+        />
+        <meta name="author" content="Fairmount Vagamon" />
       </Head>
-      < Container my = { 30} >
-        <Title order={ 2 } weight = { 100} >
+      <Container my={30}>
+        <Title order={2} weight={100}>
           Frequently Asked Questions
-            < /Title>
-            <Accordion variant="separated" mt={20}>
-        <Accordion.Item className={classes.item} value="reset-password">
-          <Accordion.Control>How can I reset my password?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
-        </Accordion.Item>
+        </Title>
+        <Accordion variant="separated" mt={20}>
+          <Accordion.Item className={classes.item} value="reset-password">
+            <Accordion.Control>How can I reset my password?</Accordion.Control>
+            <Accordion.Panel>{placeholder}</Accordion.Panel>
+          </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="another-account">
-          <Accordion.Control>Can I create more that one account?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
-        </Accordion.Item>
+          <Accordion.Item className={classes.item} value="another-account">
+            <Accordion.Control>Can I create more that one account?</Accordion.Control>
+            <Accordion.Panel>{placeholder}</Accordion.Panel>
+          </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="newsletter">
-          <Accordion.Control>How can I subscribe to monthly newsletter?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
-        </Accordion.Item>
+          <Accordion.Item className={classes.item} value="newsletter">
+            <Accordion.Control>How can I subscribe to monthly newsletter?</Accordion.Control>
+            <Accordion.Panel>{placeholder}</Accordion.Panel>
+          </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="credit-card">
-          <Accordion.Control>Do you store credit card information securely?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
-        </Accordion.Item>
+          <Accordion.Item className={classes.item} value="credit-card">
+            <Accordion.Control>Do you store credit card information securely?</Accordion.Control>
+            <Accordion.Panel>{placeholder}</Accordion.Panel>
+          </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="payment">
-          <Accordion.Control>What payment systems to you work with?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
-        </Accordion.Item>
-      </Accordion>
-            < /Container>
-            < />
+          <Accordion.Item className={classes.item} value="payment">
+            <Accordion.Control>What payment systems to you work with?</Accordion.Control>
+            <Accordion.Panel>{placeholder}</Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Container>
+    </>
   );
 }
