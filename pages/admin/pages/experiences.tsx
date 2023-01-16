43dc0a6a3164c2db6e4511f8351692a605c2c7fd
Akshay <akshay@spawoz.com>
2023-01-15 19:44:38 -0500

    new

--- pages/admin/pages/experiences.tsx
+++ pages/admin/pages/experiences.tsx
@@ -155,7 +155,7 @@ export default function Pages() {
     </Navigation>
   );
 }
-interface RowProps {
+interface RowProps { 
   data: ExperienceProps;
   handleEditAction: (data: ExperienceProps) => void;
 }
