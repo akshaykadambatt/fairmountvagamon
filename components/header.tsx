fb8167c183e1c57e0559010fbdc36dabd3dd9de0
jowinjohnchemban <jowinchemban@gmail.com>
2023-01-26 11:43:24 +0530

    Removed header.tsx tiltle

--- components/header.tsx
+++ components/header.tsx
@@ -153,7 +153,6 @@ export default function HeaderComponent() {
       <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Fairmount Resorts</title>
       <link rel="icon" type="image/png" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
