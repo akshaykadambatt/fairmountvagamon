7d9efc7c025be99bb525e75d9601634b490b32cb
jowinjohnchemban <jowinchemban@gmail.com>
2023-01-26 22:51:19 +0530

    added rel="canonical"

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,6 +168,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Welcome to Fairmount Resorts Vagamon, Kerala - Discover the Idyllic Beauty of Vagamon at Fairmount</title>
         
+        <link rel="canonical" href="https://fairmountvagamon.com/"/>
         <link rel="icon" type="image/png" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
