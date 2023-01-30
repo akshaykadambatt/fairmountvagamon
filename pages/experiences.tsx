30b54b798bcf796145cc86e44000e90112bf3540
Akshay <akshay@spawoz.com>
2023-01-30 00:58:47 -0500

    Experiences update

--- pages/experiences.tsx
+++ pages/experiences.tsx
@@ -7,17 +7,31 @@ import {
   useMantineTheme,
   Grid,
   Center,
+  createStyles,
+  Divider,
 } from "@mantine/core";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { CollectionName } from "../components/data/constants";
 import { db } from "../components/data/firebaseConfig";
+import { keyframes } from "@emotion/react";
+import { Reveal } from "react-awesome-reveal";
 import Head from "next/head";
-
 const getChild = (height: number) => <Skeleton height={height} radius="md" />;
 const BASE_HEIGHT = 360;
 const getSubHeight = (children: number, spacing: number) =>
   BASE_HEIGHT / children - spacing * ((children - 1) / children);
+  const customAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translatey(30px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translatey(0);
+  }
+`;
 
 export default function Contact() {
   const [values, setValues] = useState<ExperienceProps[]>();
@@ -27,8 +41,8 @@ export default function Contact() {
       const docRef = collection(db, CollectionName.EXPERIENCES);
       const docSnap = await getDocs(docRef);
       let data: any[] = [];
-      docSnap.forEach(element => {
-        data.push(element.data())
+      docSnap.forEach((element) => {
+        data.push(element.data());
       });
       console.log(data);
       setValues(data);
@@ -60,45 +74,75 @@ export default function Contact() {
 
         {/*<!-- Open Graph / Facebook -->*/}
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Fairmount Experiences - Explore the Natural Beauty and Cultural Treasures of Vagamon" />
-        <meta property="og:description" content="Experience the beauty and rich cultural heritage of Vagamon at Fairmount Resorts. Explore the natural landscapes and cultural treasures of this picturesque destination, with exquisite accommodation and blissful experiences. Book your stay now and discover the magic of Vagamon." />
+        <meta
+          property="og:title"
+          content="Fairmount Experiences - Explore the Natural Beauty and Cultural Treasures of Vagamon"
+        />
+        <meta
+          property="og:description"
+          content="Experience the beauty and rich cultural heritage of Vagamon at Fairmount Resorts. Explore the natural landscapes and cultural treasures of this picturesque destination, with exquisite accommodation and blissful experiences. Book your stay now and discover the magic of Vagamon."
+        />
         <meta property="og:image" content="https://fairmountvagamon.com/og-image.jpg" />
 
         {/*<!-- Twitter -->*/}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="Fairmount Experiences - Explore the Natural Beauty and Cultural Treasures of Vagamon" />
-        <meta property="twitter:description" content="Experience the beauty and rich cultural heritage of Vagamon at Fairmount Resorts. Explore the natural landscapes and cultural treasures of this picturesque destination, with exquisite accommodation and blissful experiences. Book your stay now and discover the magic of Vagamon." />
+        <meta
+          property="twitter:title"
+          content="Fairmount Experiences - Explore the Natural Beauty and Cultural Treasures of Vagamon"
+        />
+        <meta
+          property="twitter:description"
+          content="Experience the beauty and rich cultural heritage of Vagamon at Fairmount Resorts. Explore the natural landscapes and cultural treasures of this picturesque destination, with exquisite accommodation and blissful experiences. Book your stay now and discover the magic of Vagamon."
+        />
         <meta property="twitter:image" content="https://fairmountvagamon.com/og-image.jpg" />
 
         <meta name="author" content="Fairmount Vagamon" />
       </Head>
-      <Container my={30}>
+      <Container my={30} size={"md"}>
         <Title order={2} weight={100}>
           Experiences
         </Title>
-        <Box my={40}>
-          {!values&& <>
-          <Skeleton height={150} m={20} />
-          <Skeleton height={150} m={20} />
-          <Skeleton height={150} m={20} />
-          </>}
-          {values?.map((e,index)=> (
-          <Grid key={index} style={{ borderRadius: 10, border: "1px solid rgba(0 0 0 / 50% )" }} my={20} p={20}> 
-            <Grid.Col span={8} order={index%2==0?0:1}>
-              <Title order={3} weight={100}>
-              {e.name}
-              </Title>
-              <Text size={"sm"}>
-                {e.content}
-              </Text>
-            </Grid.Col>
-            <Grid.Col span={4} order={index%2==0?1:0}>
-              <img src={e.images?e.images[0]?.url:""} width={"100%"} style={{ borderRadius:10 }}/>
-              </Grid.Col>
-          </Grid>
+        <Box mb={40} mt={60}>
+          {!values && (
+            <>
+              <Skeleton height={150} m={20} />
+              <Skeleton height={150} m={20} />
+              <Skeleton height={150} m={20} />
+            </>
+          )}
+          {values?.map((e, index) => (
+            <>
+                    <Reveal keyframes={customAnimation} cascade duration={500} damping={0.3} triggerOnce={true}>
+              <Grid key={index} style={{ borderRadius: 10 }} p={20}>
+                <Grid.Col xs={12} md={8} order={index % 2 == 0 ? 0 : 1}>
+                  <Title order={3} weight={100} mb={15}>
+                    {e.name}
+                  </Title>
+                  <Text size={"sm"}>{e.content}</Text>
+                </Grid.Col>
+                <Grid.Col xs={12} md={4} order={index % 2 == 0 ? 1 : 0}>
+                <Reveal keyframes={customAnimation} cascade duration={1200} damping={0.3} triggerOnce={true}>
+                  <div className={`polaroid-item  ${index % 2 == 0 ? "polaroid-right" : "polaroid-left"}`}>
+                    <div className="polaroid-wrapper">
+                      <img src={e.images ? e.images[0]?.url : ""} width={"100%"} style={{ minHeight: "199px" }} />
+                      <Text size="xs" align="center" mt="sm">
+                        {" "}
+                        {e.name}
+                      </Text>
+                    </div>
+                  </div>
+                  </Reveal>
+                  {/* <img src={e.images?e.images[0]?.url:""} width={"100%"} style={{ borderRadius:10 }}/> */}
+                </Grid.Col>
+              </Grid>
+              <Divider mt={30} mb={40} />
+              </Reveal>
+            </>
           ))}
           <Center>
-            <Text mb={30} color={"dimmed"}>And many more...</Text>
+            <Text mb={30} size="xs" italic color={"dimmed"}>
+              And many more...
+            </Text>
           </Center>
         </Box>
       </Container>
