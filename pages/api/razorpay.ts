593d6fff72ac1b786296f12aafcdd6352580495a
Akshay <akshay@spawoz.com>
2023-01-31 02:25:40 -0500

    Razorpay setup

---
+++ pages/api/razorpay.ts
@@ -0,0 +1,38 @@
+const Razorpay = require("razorpay");
+const shortid = require("shortid");
+
+export default async function handler(req: { method: string; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: unknown): void; new(): any; }; }; }) {
+  if (req.method === "POST") {
+    // Initialize razorpay object
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+
+    // Create an order -> generate the OrderID -> Send it to the Front-end
+    const payment_capture = 1;
+    const amount = 499;
+    const currency = "INR";
+    const options = {
+      amount: (amount * 100).toString(),
+      currency,
+      receipt: shortid.generate(),
+      payment_capture,
+    };
+
+    try {
+      const response = await razorpay.orders.create(options);
+      res.status(200).json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  } else {
+      res.status(400).json(400);
+      // Handle any other HTTP method
+  }
+}
